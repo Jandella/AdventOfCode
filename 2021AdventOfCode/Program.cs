04bb8578527de966a74b26bad25ce16f456deb68
Jandella <chiara.deacetis@gmail.com>
2021-12-11 22:06:01 +0100

    Day 2 completed

--- 2021AdventOfCode/Program.cs
+++ 2021AdventOfCode/Program.cs
@@ -7,9 +7,13 @@ namespace _2021AdventOfCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Day01 d01 = new Day01();
-            Console.WriteLine("day 1 quiz 1: {0}", d01.ContaMisureQuiz1());
-            Console.WriteLine("day1 quiz 2: {0}", d01.ContaMisureQuiz2());
+            //Day01 d01 = new Day01();
+            //Console.WriteLine("day 1 quiz 1: {0}", d01.ContaMisureQuiz1());
+            //Console.WriteLine("day 1 quiz 2: {0}", d01.ContaMisureQuiz2());
+
+            Day02 d02 = new Day02();
+            Console.WriteLine("day 2 quiz 1: {0}", d02.Quiz1());
+            Console.WriteLine("day 2 quiz 2: {0}", d02.Quiz2());
             Console.ReadKey();
         }
     }
