c9080b4e5eb4914e468682ef3b29f84ee8195b68
Jandella <chiara.deacetis@gmail.com>
2021-12-11 21:44:38 +0100

    Aggiungere i file di progetto.

---
+++ 2021AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2021AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Day01 d01 = new Day01();
+            Console.WriteLine("day 1 quiz 1: {0}", d01.ContaMisureQuiz1());
+            Console.WriteLine("day1 quiz 2: {0}", d01.ContaMisureQuiz2());
+            Console.ReadKey();
+        }
+    }
+}
