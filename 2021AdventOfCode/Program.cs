a9593a9e00b75fbf67a3186b936016610b5ded91
Jandella <chiara.deacetis@gmail.com>
2021-12-12 01:47:15 +0100

    Day 3 done, added day 1 and day 2 description

--- 2021AdventOfCode/Program.cs
+++ 2021AdventOfCode/Program.cs
@@ -11,9 +11,13 @@ namespace _2021AdventOfCode
             //Console.WriteLine("day 1 quiz 1: {0}", d01.ContaMisureQuiz1());
             //Console.WriteLine("day 1 quiz 2: {0}", d01.ContaMisureQuiz2());
 
-            Day02 d02 = new Day02();
-            Console.WriteLine("day 2 quiz 1: {0}", d02.Quiz1());
-            Console.WriteLine("day 2 quiz 2: {0}", d02.Quiz2());
+            //Day02 d02 = new Day02();
+            //Console.WriteLine("day 2 quiz 1: {0}", d02.Quiz1());
+            //Console.WriteLine("day 2 quiz 2: {0}", d02.Quiz2());
+
+            Day03 d03 = new Day03();
+            Console.WriteLine("day 3 quiz 1: {0}", d03.Quiz1());
+            Console.WriteLine("day 3 quiz 2: {0}", d03.Quiz2());
             Console.ReadKey();
         }
     }
