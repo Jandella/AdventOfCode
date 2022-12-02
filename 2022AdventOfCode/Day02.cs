2e1129ebca14e9c71cbb48b712f1ceece391892b
Jandella <chiara.deacetis@gmail.com>
2022-12-02 08:14:19 +0100

    setup for day2

---
+++ 2022AdventOfCode/Day02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day02 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day02()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+
+        public Day02(string input)
+        {
+            _input = input;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
