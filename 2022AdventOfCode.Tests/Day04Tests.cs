254c14c87495b134ec1c58d80d750c377ef537d0
Jandella <chiara.deacetis@gmail.com>
2022-12-04 11:37:13 +0100

    day 4 solved

---
+++ 2022AdventOfCode.Tests/Day04Tests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day04Tests
+    {
+        private static string exampleInput = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+        [Fact]
+        public void TestDay4_SectionContains()
+        {
+            Section a = new Section("6-6");
+            Section b = new Section("4-6");
+            Assert.True(b.Contains(a));
+            Assert.False(a.Contains(b));
+
+            Section c = new Section("2-6");
+            Section d = new Section("4-8");
+            Assert.False(c.Contains(d));
+            Assert.False(d.Contains(c));
+
+            Section e = new Section("2-8");
+            Section f = new Section("3-7");
+            Assert.True(e.Contains(f));
+            Assert.False(f.Contains(e));
+        }
+
+        [Fact]
+        public async Task TestDay4_Solve1()
+        {
+            var d = new Day04(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("2", result);
+
+        }
+
+        [Fact]
+        public void TestDay4_SectionOverlaps()
+        {
+            Section a = new Section("6-6");
+            Section b = new Section("4-6");
+            Assert.True(b.Overlaps(a));
+            Assert.True(a.Overlaps(b));
+
+            Section c = new Section("2-6");
+            Section d = new Section("4-8");
+            Assert.True(c.Overlaps(d));
+            Assert.True(d.Overlaps(c));
+
+            Section e = new Section("2-8");
+            Section f = new Section("3-7");
+            Assert.True(e.Overlaps(f));
+            Assert.True(f.Overlaps(e));
+
+            Section g = new Section("2-4");
+            Section h = new Section("6-8");
+            Assert.False(g.Overlaps(h));
+            Assert.False(h.Overlaps(g));
+        }
+        [Fact]
+        public async Task TestDay4_Solve2()
+        {
+            var d = new Day04(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("4", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay4_Solve1_FileInput()
+        {
+            var d = new Day04();
+            var result = await d.Solve_1();
+            Assert.Equal("490", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay4_Solve2_FileInput()
+        {
+            var d = new Day04();
+            var result = await d.Solve_2();
+            Assert.Equal("921", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day04.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day04 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day04()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day04(string input)
+        {
+            _input = input;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            int countOverlappingSections = 0;
+            using (StringReader r = new(_input))
+            {
+                while (r.Peek() != -1)
+                {
+                    string pairs = r.ReadLine() ?? string.Empty;
+                    ElfPairSections elfPairs = new (pairs);
+                    if(elfPairs.FirstElfSection.Contains(elfPairs.SecondElfSection)
+                        || elfPairs.SecondElfSection.Contains(elfPairs.FirstElfSection))
+                    {
+                        countOverlappingSections++;
+                    }
+                }
+            }
+            return new(countOverlappingSections.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            int countOverlappingSections = 0;
+            using (StringReader r = new(_input))
+            {
+                while (r.Peek() != -1)
+                {
+                    string pairs = r.ReadLine() ?? string.Empty;
+                    ElfPairSections elfPairs = new(pairs);
+                    if (elfPairs.FirstElfSection.Overlaps(elfPairs.SecondElfSection)
+                        || elfPairs.SecondElfSection.Overlaps(elfPairs.FirstElfSection))
+                    {
+                        countOverlappingSections++;
+                    }
+                }
+            }
+            return new(countOverlappingSections.ToString());
+        }
+    }
+    //defining some classes down here to help tidy up
+    public class Section {
+        public Section()
+        {
+
+        }
+        public Section(string section)
+        {
+            var splitted = section.Split('-');
+            Start = int.Parse(splitted[0]);
+            End = int.Parse(splitted[1]);
+        }
+        public int Start { get; set; }
+        public int End { get; set; }
+        public bool Contains(Section b)
+        {
+            return Start <= b.Start && b.End <= End;
+        }
+        public bool Overlaps(Section b)
+        {
+            return (End >= b.Start && End <= b.End) 
+                || (Start >= b.Start && Start <= b.End)
+                || Contains(b);
+        }
+        public override string ToString()
+        {
+            return Start + "-" + End;
+        }
+    }
+
+    public class ElfPairSections
+    {
+        public ElfPairSections()
+        {
+            FirstElfSection = new Section();
+            SecondElfSection = new Section();
+        }
+        public ElfPairSections(string pairs)
+        {
+            var splitted = pairs.Split(",");
+            FirstElfSection = new Section(splitted[0]);
+            SecondElfSection = new Section(splitted[1]);
+        }
+        public Section FirstElfSection { get; set; }
+        public Section SecondElfSection { get; set; }
+    }
+}
