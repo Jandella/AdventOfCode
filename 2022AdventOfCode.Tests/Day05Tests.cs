4fa9c97ba46d5ecc6d08c60a883039a7e3f795d5
Jandella <chiara.deacetis@gmail.com>
2022-12-06 00:53:46 +0100

    Day 5 part 1 solved

---
+++ 2022AdventOfCode.Tests/Day05Tests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day05Tests
+    {
+        private static string exampleInput = 
+@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+        [Fact]
+        public void TestDay5_ParseStackOfCrates()
+        {
+            var a = new StackOfCrates(@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3");
+            Assert.Equal(3, a.Crates.Length);
+            Assert.Equal('N', a.Crates[0].Peek());
+            Assert.Equal('D', a.Crates[1].Peek());
+            Assert.Equal('P', a.Crates[2].Peek());
+        }
+        [Fact]
+        public void TestDay5_ParseMoves()
+        {
+            var a = new CraneInstruction("move 1 from 2 to 1");
+            Assert.Equal(1, a.Move);
+            Assert.Equal(2, a.From);
+            Assert.Equal(1, a.To);
+        }
+        [Fact]
+        public async Task TestDay5_Solve1()
+        {
+            var d = new Day05(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("CMZ", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay5_Solve2()
+        {
+            var d = new Day05(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("12", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay5_Solve1_FileInput()
+        {
+            var d = new Day05();
+            var result = await d.Solve_1();
+            Assert.Equal("TGWSMRBPN", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay5_Solve2_FileInput()
+        {
+            var d = new Day05();
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day05.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day05 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day05()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day05(string input)
+        {
+            _input = input;
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            var inputParts = _input.Split(Environment.NewLine+Environment.NewLine);
+            var crates = new StackOfCrates(inputParts[0]);
+            var instructionsSplitted = inputParts[1].Split("\n");
+            
+            foreach (var item in instructionsSplitted)
+            {
+                var currentInstruction = new CraneInstruction(item);
+                var indexToMove = currentInstruction.From - 1; //0 based
+                var destIndex = currentInstruction.To- 1;
+                for (int i = 0; i < currentInstruction.Move; i++)
+                {
+                    var crate = crates.Crates[indexToMove].Pop();
+                    crates.Crates[destIndex].Push(crate);
+                }
+            }
+            string res = "";
+            foreach (var item in crates.Crates)
+            {
+                res += item.Peek();
+            }
+            return new ValueTask<string>(res);
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StackOfCrates
+    {
+        public StackOfCrates(int size)
+        {
+            Crates = new Stack<char>[size];
+        }
+        public StackOfCrates(string input)
+        {
+            var lines = input.Split("\n");
+            var indexesLine = lines.Last();
+            var indexes = indexesLine.Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(x => int.Parse(x));
+            Crates = new Stack<char>[indexes.Count()];
+            for(int i = lines.Length - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < Crates.Length; j++)
+                {
+                    if (Crates[j] == null)
+                    {
+                        Crates[j] = new Stack<char>();
+                    }
+                    var currentChar = lines[i][(j *4) + 1];
+                    if (char.IsLetter(currentChar))
+                    {
+                        Crates[j].Push(currentChar);
+                    }
+                }
+            }
+        }
+        public Stack<char>[] Crates { get; set; }
+        
+    }
+    public class CraneInstruction
+    {
+        public CraneInstruction()
+        {
+
+        }
+        public CraneInstruction(string input)
+        {
+            //example string move 1 from 2 to 1
+            var splitted = input.Split(" ");
+            Move = int.Parse(splitted[1]);
+            From = int.Parse(splitted[3]);
+            To = int.Parse(splitted[5]);
+        }
+        public int Move { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+    }
+}
