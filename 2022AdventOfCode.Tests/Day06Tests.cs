eb1a6058b8112e959055575c4dbfe0af26607ba2
Jandella <chiara.deacetis@gmail.com>
2022-12-06 23:23:20 +0100

    Day 6 solved

---
+++ 2022AdventOfCode.Tests/Day06Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day06Tests
+    {
+        
+        [Fact]
+        public async Task TestDay6_Solve1()
+        {
+            var d = new Day06("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+            var result = await d.Solve_1();
+            Assert.Equal("7", result);
+            var e = new Day06("bvwbjplbgvbhsrlpgdmjqwftvncz");
+            var resultE = await e.Solve_1();
+            Assert.Equal("5", resultE);
+            var f = new Day06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+            var resultF = await f.Solve_1();
+            Assert.Equal("10", resultF);
+
+        }
+
+        [Fact]
+        public async Task TestDay6_Solve2()
+        {
+            var d = new Day06("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+            var result = await d.Solve_2();
+            Assert.Equal("19", result);
+            var e = new Day06("bvwbjplbgvbhsrlpgdmjqwftvncz");
+            var resultE = await e.Solve_2();
+            Assert.Equal("23", resultE);
+            var f = new Day06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+            var resultF = await f.Solve_2();
+            Assert.Equal("29", resultF);
+
+        }
+
+        [Fact]
+        public async Task TestDay6_Solve1_FileInput()
+        {
+            var d = new Day06();
+            var result = await d.Solve_1();
+            Assert.Equal("1175", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay6_Solve2_FileInput()
+        {
+            var d = new Day06();
+            var result = await d.Solve_2();
+            Assert.Equal("3217", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day06.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day06 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day06()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day06(string input)
+        {
+            _input = input;
+        }
+        public int Marker(int length)
+        {
+            var set = new List<char>();
+            bool trovato = false;
+            int index = 1;
+            using (StringReader r = new(_input))
+            {
+                while (r.Peek() != -1 && !trovato)
+                {
+                    var nextChar = Convert.ToChar(r.Read());
+
+                    set.Add(nextChar);
+
+                    if (set.Count > length)
+                    {
+                        set.RemoveAt(0);
+                    }
+
+                    if (set.Count == length)
+                    {
+                        if (set.Distinct().Count() == length)
+                        {
+                            trovato = true;
+                        }
+                    }
+
+                    if (!trovato)
+                    {
+                        index++;
+                    }
+                }
+
+            }
+            return index;
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            int index = Marker(4);
+            return new ValueTask<string>(index.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            int index = Marker(14);
+            return new ValueTask<string>(index.ToString());
+        }
+    }
+}
