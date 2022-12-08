f192c85621cfb2e645774061d6d7eaf06dce0531
Jandella <chiara.deacetis@gmail.com>
2022-12-08 23:28:02 +0100

    day7 incorrect answer

---
+++ 2022AdventOfCode.Tests/Day07Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day07Tests
+    {
+        private static string exampleInput =
+@"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+        
+        [Fact]
+        public async Task TestDay7_Solve1()
+        {
+            var d = new Day07(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("1428881", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay7_Solve2()
+        {
+            var d = new Day07(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay7_Solve1_FileInput()
+        {
+            var d = new Day07();
+            var result = await d.Solve_1();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay7_Solve2_FileInput()
+        {
+            var d = new Day07();
+            var result = await d.Solve_2();
+            Assert.Equal("10475598", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day07.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day07 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day07()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day07(string input)
+        {
+            _input = input;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            int total = 0;
+            var stack = new Stack<DeviceDirectory>();
+            var dirs = new List<DeviceDirectory>();
+            using (StringReader r = new(_input))
+            {
+                while (r.Peek() != -1)
+                {
+                    string line = r.ReadLine() ?? "-";
+                    if (line.StartsWith("$ cd"))
+                    {
+                        var dirName = line.Substring("$cd ".Length + 1);
+                        if(dirName == "..")
+                        {
+                            //go back
+                            var poppedDir = stack.Pop();
+                            if(poppedDir.Size <= 100000)
+                            {
+                                if(stack.Count > 0)
+                                {
+                                    var current = stack.Peek();
+                                    current.Size += poppedDir.Size;
+                                }
+                                //when popped, we should have the right size
+                                total+= poppedDir.Size;
+                            }
+
+                        }
+                        else
+                        {
+                            var newDir = new DeviceDirectory()
+                            {
+                                Name = dirName,
+                            };
+                            dirs.Add(newDir);
+                            if(stack.Count > 0)
+                            {
+                                // Add new directory as sub directory of current directory
+                                var current = stack.Peek();
+                                current.SubDirs.Add(newDir);
+                            }
+                            stack.Push(newDir);
+                        }
+                    }
+                    else if(!line.StartsWith("$ ls"))
+                    {
+                        //it's either a directory or a file
+                        var splitted = line.Split(' ');
+                        var current = stack.Peek();
+                        if (splitted[0] == "dir")
+                        {   
+                            var newItem = new DeviceDirectory()
+                            {
+                                Name = splitted[1],
+                            };
+                        }
+                        else
+                        {
+                            current.Size += int.Parse(splitted[0]);
+                        }
+                    }
+                    
+                }
+            }
+            var aaaa = dirs.Select(directory => directory.Size).Where(fileSize => fileSize <= 100000).Sum();
+            return new ValueTask<string>(total.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DeviceDirectory
+    {
+        public string? Name { get; set; }
+        public List<DeviceDirectory> SubDirs { get; set; } = new List<DeviceDirectory>();
+        public List<string> Files { get; set; } = new List<string>();
+        public int Size { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} ({Size})";
+        }
+    }
+}
