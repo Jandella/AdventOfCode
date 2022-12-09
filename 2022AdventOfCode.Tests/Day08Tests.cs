27692f4cce38570eb30944c792e21d13913b8f7b
Jandella <chiara.deacetis@gmail.com>
2022-12-09 14:43:09 +0100

    Setup day 8

---
+++ 2022AdventOfCode.Tests/Day08Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day08Tests
+    {
+        public string exampleInput = @"30373
+25512
+65332
+33549
+35390";
+        [Fact]
+        public async Task TestDay8_Solve1()
+        {
+            var d = new Day08(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay8_Solve2()
+        {
+            var d = new Day08(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay8_Solve1_FileInput()
+        {
+            var d = new Day08();
+            var result = await d.Solve_1();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay8_Solve2_FileInput()
+        {
+            var d = new Day08();
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day08.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day08 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day08()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day08(string input)
+        {
+            _input = input;
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
