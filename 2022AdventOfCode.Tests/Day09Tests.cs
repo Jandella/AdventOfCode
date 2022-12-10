2136bb42b9cde11b5f6f188005757db19f7ed707
Jandella <chiara.deacetis@gmail.com>
2022-12-10 22:26:29 +0100

    day9 setup

---
+++ 2022AdventOfCode.Tests/Day09Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day09Tests
+    {
+        public string exampleInput = @"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
+        [Fact]
+        public async Task TestDay9_Solve1()
+        {
+            var d = new Day09(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("13", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay9_Solve2()
+        {
+            var d = new Day09(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay9_Solve1_FileInput()
+        {
+            var d = new Day09();
+            var result = await d.Solve_1();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay9_Solve2_FileInput()
+        {
+            var d = new Day09();
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day09.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day09 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day09()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day09(string input)
+        {
+            _input = input;
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
