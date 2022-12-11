f164f46ef98891712c651fcbb4c7dc586b46abb8
Jandella <chiara.deacetis@gmail.com>
2022-12-11 22:54:58 +0100

    Day10 solved

---
+++ 2022AdventOfCode.Tests/Day10Tests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day10Tests
+    {
+        public string exampleInput = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+        [Fact]
+        public async Task TestDay10_BaseInput()
+        {
+            var d = new Day10(@"noop
+addx 3
+addx -5");
+            var result = await d.Solve_1();
+            Assert.Equal("0", result);
+        }
+        [Fact]
+        public async Task TestDay10_Solve1()
+        {
+            var d = new Day10(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("13140", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay10_Solve2()
+        {
+            var d = new Day10(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal(@"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay10_Solve1_FileInput()
+        {
+            var d = new Day10();
+            var result = await d.Solve_1();
+            Assert.Equal("17380", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay10_Solve2_FileInput()
+        {
+            var d = new Day10();
+            var result = await d.Solve_2();
+            Assert.Equal(
+@"####..##...##..#..#.####.###..####..##..
+#....#..#.#..#.#..#....#.#..#.#....#..#.
+###..#....#....#..#...#..#..#.###..#....
+#....#.##.#....#..#..#...###..#....#....
+#....#..#.#..#.#..#.#....#.#..#....#..#.
+#.....###..##...##..####.#..#.####..##..", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day10.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day10 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day10()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day10(string input)
+        {
+            _input = input;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var sum = 0;
+            using (StringReader r = new StringReader(_input))
+            {
+                var steps = 20;
+                var max = 220;
+                var cpu = new DeviceCpu();
+                var line = r.ReadLine() ?? "";
+                cpu.ReadInstruction(line);
+                while (cpu.Cycle <= max)
+                {
+                    if (cpu.CurrentInstruction == null)
+                    {
+                        if (r.Peek() == -1)
+                            break;
+
+                        line = r.ReadLine() ?? "";
+                        cpu.ReadInstruction(line);
+                    }
+
+                    if (cpu.Cycle % steps == 0)
+                    {
+                        sum += cpu.Cycle * cpu.Value;
+                        steps += 40;
+                    }
+
+                    cpu.PerformCycle();
+                }
+            }
+            return new ValueTask<string>(sum.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var device = new Device();
+            using (StringReader r = new StringReader(_input))
+            {
+                var max = 240;
+                
+                var line = r.ReadLine() ?? "";
+                device.Cpu.ReadInstruction(line);
+                while (device.Cpu.Cycle <= max)
+                {
+                    if (device.Cpu.CurrentInstruction == null)
+                    {
+                        if (r.Peek() == -1)
+                            break;
+
+                        line = r.ReadLine() ?? "";
+                        device.Cpu.ReadInstruction(line);
+                    }
+
+                    
+
+                    device.PerformCycle();
+                }
+            }
+            return new ValueTask<string>(device.Crt.Screen);
+        }
+    }
+
+    public class DeviceCpu
+    {
+        public int Cycle { get; set; } = 1;
+        public int Value { get; set; } = 1;
+
+        public CpuInstruction? CurrentInstruction { get; private set; }
+        public void PerformCycle()
+        {
+            if (CurrentInstruction == null)
+                return;  //or cycle +1 (?)
+            Cycle++;
+            if (CurrentInstruction.EndingCycle == Cycle)
+            {
+                Value += CurrentInstruction.ValueToAdd;
+                CurrentInstruction = null;
+            }
+        }
+        public void ReadInstruction(string instruction)
+        {
+            CurrentInstruction = new CpuInstruction(Cycle, instruction);
+        }
+    }
+
+    public class CpuInstruction
+    {
+        private string _toString = "";
+        public CpuInstruction(int startingCycle, string instruction)
+        {
+            _toString = instruction;
+            StartingCycle = startingCycle;
+            var splitted = instruction.Split(" ");
+            if (splitted[0] == "addx")
+            {
+                ValueToAdd = int.Parse(splitted[1]);
+                EndingCycle = startingCycle + 2;
+            }
+            else
+            {
+                EndingCycle = startingCycle + 1;
+                ValueToAdd = 0;
+            }
+        }
+        public int StartingCycle { get; private set; }
+        public int EndingCycle { get; private set; }
+        public int ValueToAdd { get; private set; }
+
+        public override string ToString()
+        {
+            return _toString;
+        }
+    }
+    public class CrtPixelPosition
+    {
+        public CrtPixelPosition()
+        {
+
+        }
+        public CrtPixelPosition(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+    public class DeviceCrt
+    {
+        public DeviceCrt()
+        {
+            Screen = "";
+
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0; c < 40; c++)
+                {
+                    ScreenChar[r, c] = '.';
+                    Screen += ".";
+                }
+                Screen += Environment.NewLine;
+            }
+
+        }
+        private char[,] ScreenChar { get; set; } = new char[6, 40];
+        public string Screen { get; private set; }
+        public int SpritePosition { get; private set; } = 1;
+        public CrtPixelPosition DrawPosition { get; set; } = new CrtPixelPosition(0, 0);
+        public void Draw(int cycle)
+        {
+            if(SpritePosition == DrawPosition.Col || SpritePosition + 1 == DrawPosition.Col
+                || SpritePosition -1 == DrawPosition.Col)
+            {
+                ScreenChar[DrawPosition.Row,DrawPosition.Col] = '#';
+            }
+            else
+            {
+                ScreenChar[DrawPosition.Row, DrawPosition.Col] = '.';
+            }
+            if (cycle % 40 == 0)
+            {
+                DrawPosition.Col = 0;
+                DrawPosition.Row++;
+            }
+            else
+            {
+                DrawPosition.Col++;
+            }
+            UpdateScreen();
+        }
+        public void UpdateSprite(int value)
+        {
+            SpritePosition += value;
+        }
+        private void UpdateScreen()
+        {
+            Screen = "";
+
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0; c < 40; c++)
+                {
+                    Screen += ScreenChar[r, c];
+                }
+                if(r < 5)
+                {
+                    Screen += Environment.NewLine;
+                }
+            }
+        }
+    }
+
+    public class Device
+    {
+        public DeviceCpu Cpu { get; set; } = new DeviceCpu();
+        public DeviceCrt Crt { get; set; } = new DeviceCrt();
+
+        public void PerformCycle()
+        {
+            Crt.Draw(Cpu.Cycle);
+            var valueToAdd = 0;
+            if(Cpu.CurrentInstruction != null)
+            {
+                valueToAdd = Cpu.CurrentInstruction.ValueToAdd;
+            }
+            
+            Cpu.PerformCycle();
+            if(Cpu.CurrentInstruction== null)
+            {
+                Crt.UpdateSprite(valueToAdd);
+            }
+        }
+    }
+}
