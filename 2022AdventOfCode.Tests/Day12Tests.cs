c77242c307eaade5c41b79af534d4c353863d534
Jandella <chiara.deacetis@gmail.com>
2022-12-12 18:44:52 +0100

    Day12 setup

---
+++ 2022AdventOfCode.Tests/Day12Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode.Tests
+{
+    public class Day12Tests
+    {
+        public string exampleInput = @"Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+        [Fact]
+        public async Task TestDay12_Solve1()
+        {
+            var d = new Day12(exampleInput);
+            var result = await d.Solve_1();
+            Assert.Equal("31", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay12_Solve2()
+        {
+            var d = new Day12(exampleInput);
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay12_Solve1_FileInput()
+        {
+            var d = new Day12();
+            var result = await d.Solve_1();
+            Assert.Equal("", result);
+
+        }
+
+        [Fact]
+        public async Task TestDay12_Solve2_FileInput()
+        {
+            var d = new Day12();
+            var result = await d.Solve_2();
+            Assert.Equal("", result);
+
+        }
+    }
+}
---
+++ 2022AdventOfCode/Day12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022AdventOfCode
+{
+    public class Day12 : AoCHelper.BaseDay
+    {
+        private string _input;
+        public Day12()
+        {
+            _input = System.IO.File.ReadAllText(InputFilePath);
+        }
+        public Day12(string input)
+        {
+            _input = input;
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
