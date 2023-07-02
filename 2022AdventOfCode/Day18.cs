c89630ab431b56d1c89e918de124e3f096363db4
Jandella <chiara.deacetis@gmail.com>
2023-07-02 15:12:04 +0200

    solve 2 not working

--- 2022AdventOfCode/Day18.cs
+++ 2022AdventOfCode/Day18.cs
@@ -41,7 +41,87 @@ namespace _2022AdventOfCode
 
         public override ValueTask<string> Solve_2()
         {
-            throw new NotImplementedException();
+            var droplets = new HashSet<LavaPoint>();
+            using (var sr = new StringReader(_input))
+            {
+                while (sr.Peek() != -1)
+                {
+                    var line = sr.ReadLine() ?? string.Empty;
+                    droplets.Add(new LavaPoint(line));
+                }
+            }
+            var freeFaces = 0;
+            var externalVolume = new Cube();
+            foreach (var lavaDrop in droplets)
+            {
+                var setOfAdjacentPoints = lavaDrop.GetAdjacentPoints();
+                var intersection = droplets.Intersect(setOfAdjacentPoints);
+                freeFaces += setOfAdjacentPoints.Count - intersection.Count();
+                externalVolume.MinX = Math.Min(lavaDrop.X, externalVolume.MinX);
+                externalVolume.MaxX = Math.Max(lavaDrop.X, externalVolume.MaxX);
+                externalVolume.MinY = Math.Min(lavaDrop.Y, externalVolume.MinY);
+                externalVolume.MaxY = Math.Max(lavaDrop.Y, externalVolume.MaxY);
+                externalVolume.MinZ = Math.Min(lavaDrop.Z, externalVolume.MinZ);
+                externalVolume.MaxZ = Math.Max(lavaDrop.Z, externalVolume.MaxZ);
+            }
+            var cubesToOutside = new HashSet<LavaPoint>();
+            var cubesInside = new HashSet<LavaPoint>();
+
+            int faceToOutside = 0;
+            foreach (var lavaDrop in droplets)
+            {
+                var setOfAdjacentPoints = lavaDrop.GetAdjacentPoints();
+                foreach (var adjacent in setOfAdjacentPoints)
+                {
+                    if(HasPathToOutside(externalVolume, adjacent, droplets, cubesToOutside, cubesInside))
+                    {
+                        cubesToOutside.Add(adjacent);
+                        faceToOutside++;
+                    }
+                    else
+                    {
+                        if (!droplets.Contains(adjacent))
+                        {
+                            cubesInside.Add(adjacent);
+                        }
+                    }
+                }
+            }
+            return new ValueTask<string>(faceToOutside.ToString());
+        }
+
+        private bool HasPathToOutside(Cube externalVolume, LavaPoint c, HashSet<LavaPoint> droplets, HashSet<LavaPoint> airOutside, HashSet<LavaPoint> airInsede)
+        {
+            var frontier = new Queue<LavaPoint>();
+            frontier.Enqueue(c);
+            var explored = new HashSet<LavaPoint>() { c };
+            while (frontier.Count > 0)
+            {
+                var currentCube = frontier.Dequeue();
+                if (airOutside.Contains(currentCube))
+                    return true;
+                if (airInsede.Contains(currentCube))
+                    continue;
+                if (droplets.Contains(currentCube))
+                    continue;
+
+                if (currentCube.X > externalVolume.MaxX || currentCube.Y > externalVolume.MaxY || currentCube.Z > externalVolume.MaxZ)
+                    return true;
+                if (currentCube.X < externalVolume.MinX || currentCube.Y < externalVolume.MinY || currentCube.Z < externalVolume.MinZ)
+                    return true;
+
+
+                var neighbours = c.GetAdjacentPoints();
+                foreach (var neighbour in neighbours)
+                {
+                    if (!explored.Contains(neighbour))
+                    {
+                        frontier.Enqueue(neighbour);
+                        explored.Add(neighbour);
+                    }
+                }
+            }
+            return false;
         }
     }
 
@@ -102,5 +182,15 @@ namespace _2022AdventOfCode
             new(0,0,-1), //behind face
             new(0,0,1) //front face
         };
+    }
+
+    public class Cube
+    {
+        public int MinX { get; set; }
+        public int MinY { get; set; }
+        public int MinZ { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public int MaxZ { get; set; }
     }
 }
